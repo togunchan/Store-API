5505a98291d70b65714d85ecfb27b62697769ca8
togunchan <togunchan@gmail.com>
2024-12-07 17:01:28 +0300

    Added product routes and controllers: - Defined routes in routes/products.js for retrieving all products and static products. - Implemented controllers in controllers/products.js for handling product data responses. - Updated app.js to use the new product routes and establish database connection.

--- app.js
+++ app.js
@@ -4,16 +4,21 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
+
 const notFoundMiddleWare = require('./middleware/not-found')
 const errorMiddleWare = require('./middleware/error-handler')
 
 // middleware
 app.use(express.json())
 
-// rootes
+// routes
 app.get('/', (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">Products Route</a>')
 })
+
+app.use('/api/v1/products', productsRouter)
 
 // products route
 app.use(notFoundMiddleWare)
@@ -24,6 +29,7 @@ const port = process.env.PORT || 5001
 const start = async () => {
   try {
     //connect DB
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, console.log(`Server is listening ${port}....`))
   } catch (error) {
     console.log(error)
