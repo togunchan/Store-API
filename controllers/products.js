2339f4af5ef583a5c8f62a90448d93647c90ddc9
togunchan <togunchan@gmail.com>
2024-12-14 17:12:14 +0300

    Enhanced getAllProducts function with sorting, field selection, and numeric filtering: - Added sort, fields, and numericFilters parameters to the function. - Implemented numeric filtering for price and rating using MongoDB operators. - Added sorting logic with default sort by createdAt. - Implemented field selection based on query parameters. - Added pagination with page and limit query parameters.

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,14 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-  const search = 'ab'
-  const products = await Product.find({
-    name: { $regex: search, $options: 'i' /* 'i' means case insensitive */ },
-  })
+  const products = await Product.find({ price: { $gt: 30 } })
+    .sort('price')
+    .select('name price')
   res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query
+  const { featured, company, name, sort, fields, numericFilters } = req.query
   const queryObject = {}
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false
@@ -23,8 +22,48 @@ const getAllProducts = async (req, res) => {
       $options: 'i' /* 'i' means case insensitive */,
     }
   }
+  if (numericFilters) {
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': '$eq',
+      '<': '$lt',
+      '<=': '$lte',
+    }
+    //regular expression
+    const regEx = /\b(>|>=|=|<|<=)\b/g
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    )
+    const options = ['price', 'rating']
+    filters = filters.split(',').forEach((item) => {
+      const [field, operator, value] = item.split('-')
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) }
+      }
+    })
+    console.log(filters)
+  }
+
   console.log(queryObject)
-  const products = await Product.find(queryObject)
+  let result = Product.find(queryObject)
+  if (sort) {
+    const sortList = sort.split(',').join(' ')
+    result = result.sort(sortList)
+  } else {
+    result = result.sort('createdAt')
+  }
+  if (fields) {
+    const fieldList = fields.split(',').join(' ')
+    result = result.select(fieldList)
+  }
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit
+  result = result.skip(skip).limit(limit)
+
+  const products = await result
   res.status(200).json({ products, nbhits: products.length })
 }
 
